11451f27ab21ebe6babc596920852359db17a3fd
Mohammad Laraib <mohammadlaraib34@gmail.com>
2023-11-11 16:05:09 +0530

    first commit

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import PropTypes from "prop-types";
+
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions,
+  disableAmount = false,
+  disableCurrency = false,
+  selectCurrency = "usd",
+  className = "",
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={disableAmount}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) =>
+            onCurrencyChange && onCurrencyChange(e.target.value)
+          }
+          disabled={disableCurrency}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+InputBox.propTypes = {
+  label: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  onAmountChange: PropTypes.func,
+  onCurrencyChange: PropTypes.func,
+  currencyOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  disableAmount: PropTypes.bool,
+  disableCurrency: PropTypes.bool,
+  selectCurrency: PropTypes.string,
+  className: PropTypes.string,
+};
+
+export default InputBox;
